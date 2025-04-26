3455e7bfd1d4516b7771a101136c2b3a3fb9ba61
Vishakha Pathak <vishakhapathak@Vishakhas-MacBook-Air-5.local>
2025-04-26 02:33:19 -0400

    fixed linear.py

--- mytorch/nn/linear.py
+++ mytorch/nn/linear.py
@@ -20,11 +20,14 @@ class Linear:
         self.W = W
         self.b = b
 
+    def __call__(self, A):
+        return self.forward(A)
+
     def forward(self, A):
         """
         :param A: Input to the linear layer with shape (*, in_features)
         :return: Output Z with shape (*, out_features)
-        
+
         Handles arbitrary batch dimensions like PyTorch
         """
         # TODO: Implement forward pass
@@ -34,21 +37,15 @@ class Linear:
 
         # Flatten all dims except last
         A_flattened = A.reshape(-1, A.shape[-1])  # (batch_size, in_features)
-        self.A = A_flattened
-        self.N = A_flattened.shape[0]
-
-        # ones vector for bias broadcast
-        self.ones = np.ones((self.N, 1))
+        self.A_flat = A_flattened  # save for backward
 
         # compute the affine transform: Z = A·W^T + b
-        Z_flat = np.dot(A_flattened, self.W.T) + np.dot(self.ones, self.b.T)
+        # bias will broadcast automatically
+        Z_flat = A_flattened.dot(self.W.T) + self.b
 
         # reshape
         out_shape = (*self.input_shape[:-1], self.W.shape[0])
         Z = Z_flat.reshape(out_shape)
-
-        # Store input for backward pass
-        self.A = A
 
         return Z
 
@@ -60,17 +57,19 @@ class Linear:
         # TODO: Implement backward pass
         # Flatten gradients as in forward
         dLdZ_flat = dLdZ.reshape(-1, dLdZ.shape[-1])
-        dLdA_flat = np.dot(dLdZ_flat, self.W)
-
 
         # Compute gradients (refer to the equations in the writeup)
-        self.dLdW = np.dot(dLdZ_flat.T, self.A)
-        self.dLdb = np.sum(dLdZ_flat, axis=0, keepdims=True).T
+        # Weight gradient: (out_features, in_features)
+        self.dLdW = dLdZ_flat.T.dot(self.A_flat)
+        # Bias gradient: (out_features,)
+        self.dLdb = np.sum(dLdZ_flat, axis=0)
 
+        # Gradient w.r.t. inputs
+        dLdA_flat = dLdZ_flat.dot(self.W)
         dLdA = dLdA_flat.reshape(self.input_shape)
 
-        if self.debug:
+        if hasattr(self, 'debug') and self.debug:
             self.dLdA = dLdA
-        
+
         # Return gradient of loss wrt input
         return dLdA
